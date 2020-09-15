4a17b4fe7bfa28c13d4ae6df1a3a2a0a60a81d22
Brenner <brenner.lattin@gmail.com>
2020-09-15 14:27:38 -0600

    Added ability to process tickets and see what ticket is next

--- hypercar/hypercar/urls.py
+++ hypercar/hypercar/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, re_path
-from tickets.views import WelcomeView, MenuView, TicketView, ProcessingView
+from tickets.views import WelcomeView, MenuView, TicketView, ProcessingView, NextView
 
 urlpatterns = [
     path("welcome/", WelcomeView.as_view()),
     path("menu/", MenuView.as_view()),
     re_path("get_ticket/(\w+)", TicketView.as_view()),
-    path("processing/", ProcessingView.as_view())
+    path("processing/", ProcessingView.as_view()),
+    path("next/", NextView.as_view())
 ]
--- hypercar/tickets/views.py
+++ hypercar/tickets/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from collections import deque
 
@@ -19,36 +19,49 @@ class MenuView(View):
 
 
 class TicketView(View):
-    line_of_cars = {"oil": 0, "tires": 0, "diagnostic": 0}
-    ticket_num = 0
-    queue = deque()
+    oil_queue = deque()
+    tires_queue = deque()
+    diag_queue = deque()
+    line_of_cars = {"oil": oil_queue, "tires": tires_queue, "diagnostic": diag_queue}
+    ticket_num = 1
+
+    # change everything so I just have one dictionary of ticket queues.
+    # For wait times, just get the length and subtract the currect request length by one for wait time
 
     def get(self, request, *args, **kwargs):
         wait = 0
-        if len(TicketView.queue) > 0:
-            TicketView.line_of_cars[TicketView.queue.popleft()] += 1
         if "change_oil" in request.path:
-            TicketView.queue.append("oil")
-            ProcessingView.tickets["oil"] += 1
-            wait += TicketView.line_of_cars["oil"] * 2
+            self.line_of_cars["oil"].append(self.ticket_num)
+            wait += (len(self.line_of_cars["oil"]) - 1) * 2
         elif "inflate_tires" in request.path:
-            TicketView.queue.append("tires")
-            ProcessingView.tickets["tires"] += 1
-            wait += TicketView.line_of_cars["oil"] * 2 + TicketView.line_of_cars["tires"] * 5
+            self.line_of_cars["tires"].append(self.ticket_num)
+            wait += len(self.line_of_cars["oil"]) * 2 + (len(self.line_of_cars["tires"]) - 1) * 5
         elif "diagnostic" in request.path:
-            TicketView.queue.append("diagnostic")
-            ProcessingView.tickets["diagnostic"] += 1
-            wait += TicketView.line_of_cars["oil"] * 2 + TicketView.line_of_cars["tires"] * 5 + TicketView.line_of_cars[
-                "diagnostic"] * 30
+            self.line_of_cars["diagnostic"].append(self.ticket_num)
+            wait += len(self.line_of_cars["oil"]) * 2 + len(self.line_of_cars["tires"]) * 5 + (len(self.line_of_cars["diagnostic"]) - 1) * 30
         else:
             raise Http404
+        context = {"ticket": self.ticket_num, "wait": wait}
         TicketView.ticket_num += 1
-        context = {"ticket": TicketView.ticket_num, "wait": wait}
         return render(request, "tickets/ticket.html", context)
 
 
 class ProcessingView(View):
-    tickets= {"oil": 0, "tires": 0, "diagnostic": 0}
-
     def get(self, request, *args, **kwargs):
-        return render(request, "tickets/processing.html", ProcessingView.tickets)
+        return render(request, "tickets/processing.html", {service: len(q) for service, q in TicketView.line_of_cars.items()})
+
+    def post(self, request, *args, **kwargs):
+        ticket = 0
+        for service in TicketView.line_of_cars:
+            if len(TicketView.line_of_cars[service]):
+                ticket = TicketView.line_of_cars[service].popleft()
+                break
+        return redirect("/processing")
+
+class NextView(View):
+    def get(self, request, *args, **kwargs):
+        for service in TicketView.line_of_cars:
+            if len(TicketView.line_of_cars[service]):
+                ticket = TicketView.line_of_cars[service][-1]
+                return HttpResponse(f"<div>Next ticket #{ticket}</div>")
+        return HttpResponse("<div>Waiting for the next client</div>")
